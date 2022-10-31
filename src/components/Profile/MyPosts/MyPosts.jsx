92cd428105c5ddb7498711ff525f67f0510a020a
Bakhrom <bahafromdsk@yandex.ru>
2022-10-31 08:56:29 +0500

    added first props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import styles from './MyPosts.module.css';
+import Post from "./Post/Post";
 const MyPosts = () => {
     return (
-        
+        <div>
+            My posts
             <div>
-                My posts
-                <div className={styles.item}>
-                    new post
-                </div>
-                <div>
-                    post 1
-                </div>
-                <div>
-                    post 2
-                </div>
+                <textarea name="txt" id="" cols="30" rows="2"></textarea>
+                <button>Add post</button>
             </div>
+            <Post message='text1' likeCount='2'/>
+            <Post message='text2' likeCount='5'/>
+            <Post message='text3' likeCount='1'/>
+        </div>
     );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     return (
         <div className={styles.content}>
             <div>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzMN1qOruU-VKfPZvGbMEOwfYeLlAUixncnjP4Vi1zrYwsxb08UpZlBJfRDC9TbSzy-yw&usqp=CAU' alt='imd' />
+                <img className={styles.contentImg} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzMN1qOruU-VKfPZvGbMEOwfYeLlAUixncnjP4Vi1zrYwsxb08UpZlBJfRDC9TbSzy-yw&usqp=CAU' alt='imd' />
             </div>
             <div>
                 ava+description
