6c0ad5d980cbb44917aa8c0a35606b9b8f23225f
Bakhrom <bahafromdsk@yandex.ru>
2022-11-01 15:57:40 +0500

    feat:add dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-// import styles from "./Dialogs.module.css"
+import styles from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return(
-        <div >Dialogs</div>
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Nika
+                </div>
+                <div className={styles.dialog}>
+                    Zida
+                </div>
+                <div className={styles.dialog}>
+                    Lara
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hello!</div>
+                <div className={styles.message}>Yo!</div>
+                <div className={styles.message}>Man!</div>
+            </div>
+        </div>
     );
 }
 
